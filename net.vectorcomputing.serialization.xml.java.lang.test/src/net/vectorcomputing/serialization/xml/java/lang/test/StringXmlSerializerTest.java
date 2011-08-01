b8f2d4a68074371dee56b7f2e71be93b8b73f0af
Lance Feagan <lfeagan@hotmail.com>
2011-07-31 23:40:31 -0500

    Fixed copyright holder to be "Lance Feagan" instead of "lfeagan"

--- net.vectorcomputing.serialization.xml.java.lang.test/src/net/vectorcomputing/serialization/xml/java/lang/test/StringXmlSerializerTest.java
+++ net.vectorcomputing.serialization.xml.java.lang.test/src/net/vectorcomputing/serialization/xml/java/lang/test/StringXmlSerializerTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2011 lfeagan
+ * Copyright 2011 Lance Feagan
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,7 +50,6 @@ public class StringXmlSerializerTest {
 		EMPTY_STRING = null;
 		SIMPLE_STRING = null;
 	}
-	
 	
 	@Test
 	public void testEmptyStringOutput() throws XmlSerializationException {
