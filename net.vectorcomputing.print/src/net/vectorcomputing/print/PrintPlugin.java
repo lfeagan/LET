1c1a1bf087f2dbe6914c2239e528515440b007f5
Lance Feagan <lfeagan@hotmail.com>
2012-03-12 20:12:20 -0500

    Added activator to the print plug-in.

---
+++ net.vectorcomputing.print/src/net/vectorcomputing/print/PrintPlugin.java
@@ -0,0 +1,58 @@
+package net.vectorcomputing.print;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import net.vectorcomputing.print.internal.ORM;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Plugin;
+import org.hibernate.SessionFactory;
+import org.osgi.framework.BundleContext;
+
+public class PrintPlugin extends Plugin {
+
+	public static final String PLUGIN_ID = "net.vectorcomputing.photo"; //$NON-NLS-1$
+
+	private static BundleContext context;
+	private static PrintPlugin plugin;
+
+	public static PrintPlugin getPlugin() {
+		return plugin;
+	}
+	
+	static BundleContext getContext() {
+		return context;
+	}
+	
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		super.start(bundleContext);
+		PrintPlugin.context = bundleContext;
+		PrintPlugin.plugin = this;
+		ORM.start(getHibernateCfg());
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		PrintPlugin.context = null;
+		PrintPlugin.plugin = null;
+		ORM.stop();
+		super.stop(bundleContext);
+	}
+	
+	public static URL getBundleURL() throws IOException {
+		// Retrieve the URL to the root of the plug-in
+		return FileLocator.toFileURL(plugin.getBundle().getEntry("/")); //$NON-NLS-1$
+	}
+	
+	private static final String getHibernateCfg() throws IOException {
+		return getBundleURL().getPath() + "hibernate.cfg.xml";
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return ORM.getSessionFactory();
+	}
+	
+}
